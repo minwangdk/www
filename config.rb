734701cadb557acd67fb5f3db8f3dd0f2fc8aeda
minwangdk <minwangdk@gmail.com>
2014-05-26 16:18:02 +0200

    header

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-
+require 'compass-h5bp'
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
--- user_login_form.php
+++ user_login_form.php
@@ -12,8 +12,16 @@ if(isset($_SESSION['loginID']))
 
 
 <form name="input" action="sql_user_login.php" method="get">
-Username: <input type="text" name="username"></br>
-Password: <input type="password" name="password">
+	Username: <label>Username</label><input type="text" name="username"></br>
+	Password: <label>Password</label><input type="password" name="password">
+
+	<div class="checkbox">
+        <input id="remember" type="checkbox"> <label for=
+        "remember">Remember me on this computer</label>
+    </div>
+
+    
+ 
 <input type="submit" value="Submit">
 </form>
 
